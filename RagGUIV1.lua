1845c23c135ac6328459b8525056db6c67f132a4
MrLosSantos <2012830784@isiswa.uitm.edu.my>
2020-07-10 13:20:00 +0100

    Update RagGUIV1.lua

--- RagGUIV1.lua
+++ RagGUIV1.lua
@@ -7,6 +7,8 @@ local ScreenGui = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
 local TextButton = Instance.new("TextButton")
 local TextButton_2 = Instance.new("TextButton")
+local TextButton_3 = Instance.new("TextButton")
+local TextButton_4 = Instance.new("TextButton")
 
 --Properties:
 
@@ -16,8 +18,8 @@ ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 Frame.Parent = ScreenGui
 Frame.BackgroundColor3 = Color3.fromRGB(59, 59, 59)
 Frame.BorderSizePixel = 6
-Frame.Position = UDim2.new(0, 826, 0, 0)
-Frame.Size = UDim2.new(0, 270, 0, 165)
+Frame.Position = UDim2.new(0, 288, 0, 92)
+Frame.Size = UDim2.new(0, 508, 0, 186)
 
 TextButton.Parent = Frame
 TextButton.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
@@ -43,9 +45,35 @@ TextButton_2.TextScaled = true
 TextButton_2.TextSize = 14.000
 TextButton_2.TextWrapped = true
 
+TextButton_3.Parent = Frame
+TextButton_3.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
+TextButton_3.BorderSizePixel = 5
+TextButton_3.Position = UDim2.new(0, 281, 0, 85)
+TextButton_3.Size = UDim2.new(0, 200, 0, 50)
+TextButton_3.Font = Enum.Font.SourceSans
+TextButton_3.Text = "FLING"
+TextButton_3.TextColor3 = Color3.fromRGB(157, 255, 0)
+TextButton_3.TextScaled = true
+TextButton_3.TextSize = 14.000
+TextButton_3.TextStrokeColor3 = Color3.fromRGB(35, 73, 120)
+TextButton_3.TextWrapped = true
+
+TextButton_4.Parent = Frame
+TextButton_4.BackgroundColor3 = Color3.fromRGB(104, 104, 104)
+TextButton_4.BorderSizePixel = 5
+TextButton_4.Position = UDim2.new(0.57874012, 0, 0.0913978517, 0)
+TextButton_4.Size = UDim2.new(0, 200, 0, 50)
+TextButton_4.Font = Enum.Font.SourceSans
+TextButton_4.Text = "IY"
+TextButton_4.TextColor3 = Color3.fromRGB(157, 255, 0)
+TextButton_4.TextScaled = true
+TextButton_4.TextSize = 14.000
+TextButton_4.TextStrokeColor3 = Color3.fromRGB(35, 73, 120)
+TextButton_4.TextWrapped = true
+
 -- Scripts:
 
-local function UPYH_fake_script() -- TextButton.Script 
+local function FMEMQ_fake_script() -- TextButton.Script 
 	local script = Instance.new('Script', TextButton)
 
 	function parentPressed()
@@ -53,8 +81,8 @@ local function UPYH_fake_script() -- TextButton.Script
 	end
 	script.Parent.MouseButton1Down:Connect(parentPressed)
 end
-coroutine.wrap(UPYH_fake_script)()
-local function KVSXKA_fake_script() -- TextButton_2.Script 
+coroutine.wrap(FMEMQ_fake_script)()
+local function BJVTID_fake_script() -- TextButton_2.Script 
 	local script = Instance.new('Script', TextButton_2)
 
 	function parentPressed()
@@ -62,8 +90,8 @@ local function KVSXKA_fake_script() -- TextButton_2.Script
 	end
 	script.Parent.MouseButton1Down:Connect(parentPressed)
 end
-coroutine.wrap(KVSXKA_fake_script)()
-local function XOTYXI_fake_script() -- Frame.LocalScript 
+coroutine.wrap(BJVTID_fake_script)()
+local function EOJJELI_fake_script() -- Frame.LocalScript 
 	local script = Instance.new('LocalScript', Frame)
 
 	local UserInputService = game:GetService("UserInputService")
@@ -106,4 +134,22 @@ local function XOTYXI_fake_script() -- Frame.LocalScript
 		end
 	end)
 end
-coroutine.wrap(XOTYXI_fake_script)()
+coroutine.wrap(EOJJELI_fake_script)()
+local function NWNBLBO_fake_script() -- TextButton_3.Script 
+	local script = Instance.new('Script', TextButton_3)
+
+	function parentPressed()
+		loadstring(game:HttpGet("https://pastebin.com/raw/VghwJDq2",true))()
+	end
+	script.Parent.MouseButton1Down:Connect(parentPressed)
+end
+coroutine.wrap(NWNBLBO_fake_script)()
+local function TYUGFAK_fake_script() -- TextButton_4.Script 
+	local script = Instance.new('Script', TextButton_4)
+
+	function parentPressed()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+	end
+	script.Parent.MouseButton1Down:Connect(parentPressed)
+end
+coroutine.wrap(TYUGFAK_fake_script)()
